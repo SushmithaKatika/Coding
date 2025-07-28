7e0ebb8accb923685b52eca088ca9abe4f0fe5dd
Sushmitha Katika <katikasushmitha7228@gmail.com>
2025-07-28 21:35:02 +0530

    Daily update - 28-07-2025

---
+++ NumberOfGoodPairs.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int numIdenticalPairs(int[] nums) {
+        HashMap <Integer,Integer> hm =new HashMap<>();
+        int count = 0;
+        for(int i=0;i<nums.length;i++){
+            int key = nums[i];
+            hm.put(key,hm.getOrDefault(key,0)+1);
+        }
+        int ans = 0;
+        for(int bom:hm.keySet()){
+            int val=hm.get(bom);
+            ans+= ((val-1)*val)/2;
+        }
+        return ans;
+    }
+}
